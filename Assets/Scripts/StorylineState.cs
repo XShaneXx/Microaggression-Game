2bb8d98ccc3a9196b07dc3a6ed80a7a1458d2191
XShaneXx <xuxufeng294@gmail.com>
2024-08-12 16:25:53 -0400

    Content_01

---
+++ Assets/Scripts/StorylineState.cs
@@ -0,0 +1,8 @@
+using System.Collections.Generic;
+
+public static class StorylineState
+{
+    public static bool hasCheckedMedicalBottle = false; // Player checked the medical bottle
+    public static bool hasTalkedToFirstNPC = false; // Player talked to the first NPC
+    public static bool hasUnlockedNextNPCDialogue = false; // Player unlocked the next NPC dialogue
+}
